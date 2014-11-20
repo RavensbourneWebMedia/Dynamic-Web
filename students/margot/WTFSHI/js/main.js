cf87430f60be5452ed1a43314bfdd4c0bcd1f394
Myrtilletamere <margot_mc@hotmail.fr>
2014-11-20 11:56:48 +0000

    yay

--- students/margot/WTFSHI/js/main.js
+++ students/margot/WTFSHI/js/main.js
@@ -1,1 +1,25 @@
+console.log("'til the end of the line")
 
+//$( "enter" ).on("enter", enterFunction )
+
+//function enterFunction() {
+//	var target = 
+//}
+
+
+
+//$('.block1	').click( function() {
+//	var target = $(this).children(".description");
+//	if ( target .css ("display") == "none") {
+//		target .css ("display", "block");
+//	}
+//	else {
+//		target .css ("display", "none");
+//	}
+	//});
+
+$("#hide").click(function(){
+  $("#welcome").hide("slow");
+  $("#time").show("slow")
+});
+//When clicking "enter" button, first page disappears & time demand appears

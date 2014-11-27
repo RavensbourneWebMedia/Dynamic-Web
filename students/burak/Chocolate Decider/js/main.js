ba52199f7d1032aad6e404a464e39c698227ea89
Burak Ozdemir <b.ozdemir@students.rave.ac.uk>
2014-11-27 14:30:44 +0000

    Progressing

--- students/burak/Chocolate Decider/js/main.js
+++ students/burak/Chocolate Decider/js/main.js
@@ -10,46 +10,145 @@ spreadsheetURL += "public/values?alt=json" //we want JSON!
 console.log(spreadsheetURL)
 
 var spreadsheetRows // declare this variable outside of the $.getJSON function so that it will be accessible anywhere
+var products = [] // empty array
+var filteredProducts = [] // empty array
 
 //use jQuery to get a JSON file from a certain URL
 //once we get that file, do something 
-$.getJSON(spreadsheetURL, function(result){
-	console.log(result)
+$.getJSON(spreadsheetURL, function(result)
+{
+	// console.log(result)
 
 	spreadsheetRows = result.feed.entry
-	console.log(spreadsheetRows)
-	$.each(spreadsheetRows, function(index, student)
-	{
-		
-		// var product = //grab the student's name
-		// var a =  //grab the student's animal
-		// console.log(name + " would like to have a pet " + animal) 
-		// var sentence = name + " would like to have a pet " + animal
-		// var li = "<li>" + sentence + "</li>"
-		// $("ul#likes").append(li)
-		
+	// console.log(spreadsheetRows)
 
+	// loop through all rows to populate our products array
+	$.each(spreadsheetRows, function(index, row)
+	{
+		// console.log(row)
+		var product = 
+		{
+			name: row.gsx$name.$t,
+			contains:
+			{
+				dairy: row.gsx$dairy.$t,
+				nuts: row.gsx$nuts.$t
+				// TODO add all other allergies
+			},
+			moods:
+			{
+				dark: Number(row.gsx$darkmood.$t) // convert to number
+				// TODO add all other moods
+			}
+		}
+		products.push(product)
 	})
+
+	console.log(products)
 
 	$("#loading").fadeOut()
 })
+
+// function to filter products by allergies
+function filterProducts()
+{
+	// see which options are checked
+	var checkedInputs = $('#allergies input:checked')
+	var filtersToCheck = []
+	// loop through checked inputs to populate filtersToCheck
+	checkedInputs.each(function(index, input)
+	{
+		// console.log(input.value)
+		filtersToCheck.push(input.value)
+	})
+	console.log(filtersToCheck)
+
+	if (filtersToCheck.length == 0) // if there are no filtersToCheck
+	{
+		filteredProducts = products // all products are eligible
+	}	
+	else
+	{
+		// loop through the products to populate the filteredProducts array
+		$.each(products, function(index, product)
+		{
+			// console.log(product)
+
+			// flag, we assume it's true until proven false
+			var matchesAllFilters = true
+
+			// loop through the filtersToCheck
+			// to see if the product matches all of them
+			$.each(filtersToCheck, function(index, filter)
+			{
+				// console.log(filter)
+				var productContains = product.contains[filter] // yes or no
+				console.log('Does ' + product.name + ' contains ' + filter + '? ' + productContains)
+
+				if (productContains == 'yes') 
+				{
+					// we filter it out
+					matchesAllFilters = false
+				}
+				else
+				{
+					// do nothing		
+				}
+			})
+
+			// after the loop
+			// if the product still matches all filters
+			// we add it to the filteredProducts
+			if (matchesAllFilters == true)
+			{
+				// we "keep" the product
+				filteredProducts.push(product)
+			}	
+		})
+
+		console.log(filteredProducts)
+
+
+
+
+
+	}	 
+}
+
+// function to rank products by mood
+function rankProducts()
+{
+	// loop through the filteredProducts
+
+	displayResults()
+}
+
+// display the results
+function displayResults()
+{
+
+}
    
-   $("#go").click(function(){
-  $("#firstpage").hide();
-  $("#secondpage").show();
-});
+// navigation   
+	$("#go").click(function(){
+	  $("#firstpage").hide();
+	  $("#secondpage").show();
+	});
 
-     $("#next").click(function(){
-  $("#secondpage").hide();
-  $("#thirdpage").show();
-});
+	$("#next").click(function()
+	{
+	  // $("#secondpage").hide();
+	  // $("#thirdpage").show();
+	  filterProducts()
+	});
 
-         $("#next2").click(function(){
-  $("#thirdpage").hide();
-  $("#fourthpage").show();
-});
+	$("#next2").click(function(){
+	  $("#thirdpage").hide();
+	  $("#fourthpage").show();
+	  rankProducts()
+	});
 
- $("#startagain").click(function(){
-  $("#fourthpage").hide();
-  $("#firstpage").show();
-});
+	$("#startagain").click(function(){
+	  $("#fourthpage").hide();
+	  $("#firstpage").show();
+	});

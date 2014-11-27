1ac948daf11c26d018483644acde5f44427c509c
Myrtilletamere <margot_mc@hotmail.fr>
2014-11-27 18:51:22 +0000

    Update: css up & functionning API (rottentomatoes)

--- students/margot/WTFSHI/js/main.js
+++ students/margot/WTFSHI/js/main.js
@@ -22,8 +22,55 @@ $("#hide").click(function(){
   $("#welcome").hide("slow");
   $("#time").show("slow")
 });
-$("#show").click(function(){
+$("#tv").click(function(){
   $("#time").hide("slow");
-  $("#pageShow").show("slow")
+  $("#pagetv").show("slow")
 });
-//When clicking "enter" button, first page disappears & time demand appears
+$("#movie").click(function(){
+  $("#time").hide("slow");
+  $("#moviepage").show("slow")
+});
+
+
+function pad(number, length) {
+  var str = '' + number;
+  while(str.length < length) {
+    str = '0' + str;
+  }
+  return str;
+}
+
+$.ajax(
+{
+  dataType: 'jsonp',
+  url:"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=z9g8xee26dpu2j8vkfekc93q&country=uk&callback=onRTBoxOfficeSuccess"
+})
+
+function onRTBoxOfficeSuccess(data) 
+{
+  console.log(data)
+  var randomMovie = data.movies[Math.floor(Math.random()*data.movies.length)]
+
+  console.log(randomMovie)
+}
+
+
+/*$.getJSON("http://www.omdbapi.com/?i=tt"+movie, function(data){
+   console.log(data)
+    var title = data.Title;
+    var year = data.Year;
+    var rating = data.imdbRating;
+    var actors = data.Actors;
+    var plot = data.Plot;
+    var link = 'http://www.imdb.com/title/tt'+movie+'/';
+    $("#title").text(title);
+    $("#year").text(year);
+    $("#rating").text(rating);
+    $("#actors").text(actors);
+    $("#plot").text(plot);
+    $("#link a").attr("href", link);
+    console.log(title + year);
+  })
+*/
+
+

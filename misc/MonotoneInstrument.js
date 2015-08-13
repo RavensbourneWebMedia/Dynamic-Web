8d3d8e85a79ccce8633105347f93520c64fcfda9
Matteo Menapace <hello@baddeo.com>
2015-08-13 20:55:36 +0100

    Y is for volume

--- misc/MonotoneInstrument.js
+++ misc/MonotoneInstrument.js
@@ -1,13 +1,23 @@
 function MonotoneInstrument( audioContext )
 {
 	this.context = audioContext
+
+	this.oscillator = undefined // we'll define it in this.play()
+	
+	// Gain is a node to control the output volume
+	// see https://developer.mozilla.org/en-US/docs/Web/API/GainNode
+	this.volume = this.context.createGain() 
 	
 	this.play = function( frequency, stopWhen )
 	{
 		if (typeof this.oscillator == 'undefined') // which means "if this.oscillator doesn't exist"
 		{
 			this.oscillator = a.createOscillator()
-			this.oscillator.connect( this.context.destination )
+
+			// connections: oscillator → volume → destination
+			this.oscillator.connect( this.volume )
+			this.volume.connect( this.context.destination )
+
 			this.oscillator.start(0)
 		}	
 		this.setFrequency( frequency )
@@ -36,5 +46,14 @@ function MonotoneInstrument( audioContext )
 		{
 			this.oscillator.frequency.value = frequency
 		}	
-	}	
+	}
+
+	this.setVolume = function( volume )
+	{
+		this.volume.gain.value = volume
+	}
+
+	/* TODO 
+	this.setNote() + noteToFrequency()	
+	*/
 }

67a197f318a0bd56c63a153e5e3eb5bc371e6bdc
thisisthor <t.njamo@rave.ac.uk>
2017-10-30 19:18:13 +0000

    added code

---
+++ misc/DynamicWeb-v2/js/firebase.js
@@ -0,0 +1,22 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDqCowWm_tfPP6BlQZIWiRCWzSwsD1dnFM",
+    authDomain: "dynamic-web-template-e91d4.firebaseapp.com",
+    databaseURL: "https://dynamic-web-template-e91d4.firebaseio.com",
+    projectId: "dynamic-web-template-e91d4",
+    storageBucket: "dynamic-web-template-e91d4.appspot.com",
+    messagingSenderId: "416580094616"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+var peopleDatabase = database.ref('people');
+var peopleList = [];
+
+peopleDatabase.on('child_added', function( firebaseObject )
+{
+	var person = firebaseObject.val();
+  	peopleList.push(person);
+  	// "push" is JavaScript's lingo for "add to a list"
+})
--- misc/DynamicWeb-v2/js/main.js
+++ misc/DynamicWeb-v2/js/main.js
@@ -9,3 +9,21 @@ $('#back').click( function(){
   $('#home').show();
   $('#details').hide();
 });
+
+// use jQuery to select the HTML elements we're going to manipulate
+var homeGoButton = $('#home button');
+var homeDropdown = $('#home select');
+var resultsOL = $('#results ol');
+// tell the GO button to do something when we click it
+homeGoButton.click( function()
+{
+  // get user input
+  var selectedOption = homeDropdown.val();
+  // using jQuery val(), see http://api.jquery.com/val
+  console.log('You picked ' + selectedOption);
+	// filter+sort people by user selection
+  	var resultsList = filterAndSortList(peopleList, selectedOption);
+  	console.log(resultsList);
+    // show the results in the #results section
+  	showList(resultsList, resultsOL);
+})
